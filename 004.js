141cf6432118d3ab1ac7494489560d2490178585
Ignacio Iacovino <nacho.iacovino@gmail.com>
2020-02-04 04:09:04 +0100

    Add one challenge, fix empty file

--- 004.js
+++ 004.js
@@ -1,3 +1,30 @@
-const friend = friends => friends.filter(i => i.length === 4)
+const persistence2 = num => {
+    let result = num
+    let count = 0
+    while (result.toString().length !== 1) {
+        let digits = result.toString().split("")
+        result = 1
+        count++
+        for (let i in digits) {
+            result *= parseInt(digits[i])
+        }
+    }
 
-console.log(friend(["Max", "Rachel", "Dick", "Chris", "Bart", "James", "Ryan", "Boris", "Claire", "Lisa", "Tim", "Frank", "Arnold", "Bruce", "Nikola", "JoJo", "Ivan", "Alex", "Omar"]))
+    return count
+}
+
+// Best solution
+const persistence = num => {
+    let count = 0
+    num = num.toString()
+    
+    while (num.length > 1) {
+      count++
+      num = num.split("").map(Number).reduce((a, b) => a * b).toString()
+    }
+    
+    return count
+}
+
+
+console.log(persistence(999))
